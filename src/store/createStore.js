a4843e3f4d142d221f5b75131cf9f7ca4817547d
Richard Vašek <vasekrichard@seznam.cz>
2016-06-05 12:27:24 +0200

    chore(store): improve createStore readability

---
+++ src/store/createStore.js
@@ -0,0 +1,40 @@
+import { applyMiddleware, createStore } from 'redux'
+import { routerMiddleware } from 'react-router-redux'
+import thunk from 'redux-thunk'
+import makeRootReducer from './reducers'
+
+export default (initialState = {}, history) => {
+  // ======================================================
+  // Middleware Configuration
+  // ======================================================
+  const middleware = [thunk, routerMiddleware(history)]
+
+  // ======================================================
+  // Developer Tools
+  // ======================================================
+  if (__DEBUG__) {
+    const devToolsExtension = window.devToolsExtension
+    if (typeof devToolsExtension === 'function') {
+      middleware.push(devToolsExtension())
+    }
+  }
+
+  // ======================================================
+  // Store Instantiation and HMR Setup
+  // ======================================================
+  const store = createStore(
+    makeRootReducer(),
+    initialState,
+    applyMiddleware(...middleware)
+  )
+  store.asyncReducers = {}
+
+  if (module.hot) {
+    module.hot.accept('./reducers', () => {
+      const reducers = require('./reducers').default
+      store.replaceReducer(reducers)
+    })
+  }
+
+  return store
+}
